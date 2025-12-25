4ccc87d4d19b39efe8562c22257b367424b95179
Guruji-24 <pateltarun8808@gmail.com>
2025-12-25 23:57:46 +0530

    Add files via upload

---
+++ dice.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int dn() {
+    return (rand() % 6)+1;
+	}
+int main()
+{
+	for(int i=0;i<10;i++){
+    printf("The outcome is:%d\n",dn());}
+    return 0;
+}
